93455ef9924a01168f222e2ee36dc31eeb084ddb
Tom Wolf <tom@t-dub.net>
2025-02-17 11:56:17 -0600

    test: add duplicate test for desktop arg

--- test/test.js
+++ test/test.js
@@ -10,4 +10,16 @@ describe('Tests for Useragents.me', () => {
     assert.ok(typeof res[0].ua === 'string');
     assert.ok(typeof res[0].pct === 'number');
   });
+
+  it(
+      'desktop should also return an array of objects with user agents and pct',
+      async () => {
+        const res = await useragentsme('desktop');
+        assert.ok(res.length > 0);
+        assert.ok(Object.hasOwn(res[0], 'ua'));
+        assert.ok(Object.hasOwn(res[0], 'pct'));
+        assert.ok(typeof res[0].ua === 'string');
+        assert.ok(typeof res[0].pct === 'number');
+      },
+  );
 });
