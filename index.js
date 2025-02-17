05fc9ddc8336b2a14cb40b97e29a5209e2da3f94
Tom Wolf <tom@t-dub.net>
2025-02-17 11:47:26 -0600

    feat: add support for fetching desktop agents too
    Add support for fetching the desktop agent list as well via passing an
    argument.  Retains default behavior of fetching mobile agents if no
    argument passed.

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ const cheerio = require('cheerio');
 
 // This is just a wrapper for https://www.useragents.me/ APIs
 const website = 'https://www.useragents.me/';
-const selectorId = '#most-common-mobile-useragents-json-csv';
-const selector = `${selectorId} > div:nth-child(1) > textarea`;
+const selectorIds = {
+  mobile: '#most-common-mobile-useragents-json-csv',
+  desktop: '#most-common-desktop-useragents-json-csv',
+}
 
 /**
  * Scrape User Agents from useragents.me website textarea
+ * @param {string} [uaType=mobile] - Specify type of agents: mobile, desktop
  * @return {Promise<Array>} Array of objects
  */
-async function getJsonFromPage() {
+async function getJsonFromPage(uaType = 'mobile') {
+  // Protect against UA types we do not have selectors for
+  if (!['mobile', 'desktop'].includes(uaType)) {
+    throw new Error(`Unsupported user-agent type specified: ${uaType}`);
+  }
+
+  const selector = `${selectorIds[uaType]} > div:nth-child(1) > textarea`;
+  console.error('selector', selector);
+
   try {
     const { data } = await axios.get(website);
     const $ = cheerio.load(data);
@@ -23,12 +34,13 @@ async function getJsonFromPage() {
 
 /**
  * Get User Agents from useragents.me website as JSON
+ * @param {string} [uaType=mobile] - Specify type of agents: mobile, desktop
  * @return {Promise<Array>} Array of objects such as:
  * [{ ua: string, pct: number }, ...]
  */
-async function useragentsme() {
+async function useragentsme(uaType = 'mobile') {
   try {
-    const userAgents = await getJsonFromPage();
+    const userAgents = await getJsonFromPage(uaType);
     return userAgents;
   } catch (error) {
     console.error(error);
